12f025f25a2fe4086f5c0219160269631c01c362
agam <agamgupta1988@gmail.com>
2019-08-15 10:17:06 -0500

    changed gateway  url and favicon

--- src/app/api/api-configuration.ts
+++ src/app/api/api-configuration.ts
@@ -8,7 +8,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = '//10.0.176.53:8081/productmanagement';
+  rootUrl: string = '//rsgconsole.net:8089/productmanagement';
 }
 
 export interface ApiConfigurationInterface {
